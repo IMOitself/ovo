4165e58f6d44283ed874bf2b73e66fb51fc4b1f4
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-07-28 22:49:48 +0800

    feat: add base blinking logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 
-function blink(){
-  
-}
-
 export default function App() {
-  const [isEyesOpen, setIsEyesOpen] = useState(false);
+  const [isEyesOpen, setIsEyesOpen] = useState(0);
+  const [styleDisplay, setStyleDisplay] = useState('none');
+
+  function blink(){
+    setIsEyesOpen(isEyesOpen == 0 ? 1 : 0)
+    setStyleDisplay(isEyesOpen == 0 ? 'flex' : 'none')
+  }
 
   return (
     <div className="bg">
       <div className="ovo">
-        <img className="ovo-open" src={"bit_def.png"} style={{display: 'none'}} />
+        <img className="ovo-open" src={"bit_def.png"} style={{display: {styleDisplay}}} />
         <img className="ovo-close" src={"bit_blinks.png"}/>
       </div>
 
-      sweet dreams...
+      <button onClick={blink}>{isEyesOpen}</button>
     </div>
   )
 }
