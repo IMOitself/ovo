d5ec2dd68111039cc9b7b22f69323fe1174706e1
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-07-28 22:30:47 +0800

    feat: smaller bite sized ovo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+function blink(){
+  
+}
+
 export default function App() {
+  const [isEyesOpen, setIsEyesOpen] = useState(false);
+
   return (
     <div className="bg">
       <div className="ovo">
+        <img className="ovo-open" src={"bit_def.png"} style={{display: 'none'}} />
         <img className="ovo-close" src={"bit_blinks.png"}/>
-        <img className="ovo-open" src={"bit_def.png"}/>
       </div>
 
-      i wonder when will it wake up..
+      sweet dreams...
     </div>
   )
 }
