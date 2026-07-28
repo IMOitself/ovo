aa6c37ef705f69ca1b9c634584401e3f5983c06c
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-07-28 22:16:06 +0800

    feat: add and hide ovo's awoken face

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import './App.css'
 export default function App() {
   return (
     <div className="bg">
-      <img className="ovo" src={"bit_blinks.png"}/>
-      shh its sleeping..
+      <div className="ovo">
+        <img className="ovo-close" src={"bit_blinks.png"}/>
+        <img className="ovo-open" src={"bit_def.png"}/>
+      </div>
+
+      i wonder when will it wake up..
     </div>
   )
 }
