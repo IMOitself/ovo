ac45a40fa466175b0a9a4e67cad93eab03432744
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-07-30 09:31:10 +0800

    feat: hide/show input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+let typingTimeout = null;
 
 export default function App() {
   const [isEyesOpen, setIsEyesOpen] = useState(0);
   const [styleDisplay, setStyleDisplay] = useState('none');
-  const typingTimeout = null;
+  const [inputText, setInputText] = useState('');
 
   function blink(){
     setIsEyesOpen(isEyesOpen == 0 ? 1 : 0)
     var x = document.querySelector(".ovo-open");
     x.style.display = isEyesOpen == 0 ? "block" : "none";
+  }
+
+  function handleInputOnTyping(){
+    var ovo_input = document.querySelector(".ovo-input");
+    ovo_input.style.display = 'block';
+    clearTimeout(typingTimeout)
+    typingTimeout = setTimeout(() => {
+      setInputText('')
+      ovo_input.style.display = 'none';
+      document.querySelector(".input").blur()
+    }, 2000)
+  }
+
+  function handleInputOnChange(event){
+        setInputText(event.target.value);
   }
 
   const myTimeout = setTimeout(blink, 1000); // IDK WHY IT IS BLINKING CONSTANTLY
@@ -21,7 +38,7 @@ export default function App() {
         <img className="ovo-open" src={"bit_def.png"} />
         <img className="ovo-close" src={"bit_blinks.png"}/>
       </div>
-      <input type='text'/>
+      <input className="input" type='text' value={inputText} onChange={handleInputOnChange} onKeyDown={handleInputOnTyping} onFocus={handleInputOnTyping}/>
     </div>
   )
 }
